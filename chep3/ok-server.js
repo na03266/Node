41116294288b5a55803533d368be5858cc0c1671
NaHwangje <na03266@naver.com>
2023-09-06 14:39:52 +0900

    라우터 테스트

--- chep3/ok-server.js
+++ chep3/ok-server.js
@@ -1,7 +1,21 @@
 const http = require("http");
-const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html"); // 응답의 헤더 설정
-    res.end("OK");
-});
+const url = require("url");
 
-server.listen("3000", () => console.log("OK, 서버 시작!")); // 접속 대기
+http
+.createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname; //패스 명 할당
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    if (path == "/user"){
+        res.end("[user] name ㅎㅎ: andy, age : 30"); // /user 결과값 설정
+    } else if (path == "/feed"){
+        res.end(`<ul>
+        <li>prcture1</li>
+        <li>prcture2</li>
+        <li>prcture3</li>
+        </ul>`); //  /feed에 대한 결과값 설정
+    } else{
+        res.statusCode = 404;
+        res.end("404 page not found");
+    }
+}).listen("3000", ()=> console.log("라우터를 만들어보자!"))
