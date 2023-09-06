901617e482f848a798b410fa58d0dbb629b34eb7
NaHwangje <na03266@naver.com>
2023-09-06 14:28:55 +0900

    서버 요청 커맨드..

--- chep2/test_hello.js
+++ chep2/test_hello.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 
 export const options = { //테스트 옵션
-    vus: 2000,  // 가상유저를 설정하는 항목
+    vus: 200,  // 가상유저를 설정하는 항목
     duration: "10s", // 몇초동안 테스트할지 선택
 };
 
---
+++ chep3/ok-server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html"); // 응답의 헤더 설정
+    res.end("OK");
+});
+
+server.listen("3000", () => console.log("OK, 서버 시작!")); // 접속 대기
